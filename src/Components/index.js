b27b16abf14eccd969bc29d02a40a9b3a972a201
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-24 18:06:06 +0530

    Header Part

---
+++ src/Components/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div className="logo_container servicelogo">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src='public/logo192.png'
+          width="243"
+          height="52"
+          class="d-inline-block align-top" alt
+        />
+        Maveric Connect
+      </a>
+    </nav>
+    </div>
+  );
+};
+
+export default Header;
