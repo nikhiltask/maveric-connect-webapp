dedd3b7893210e413ed238e643130ed67a3c1aef
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-25 09:47:30 +0530

    Post branch

---
+++ src/Pages/PostPage/Post.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+const PostPage = () => {
+  return (
+    <div class="card">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <ul class="list-group list-group-flush">
+                <li class="media">
+                  <img
+                    class="mr-3"
+                    src="maveric-logo-updated.png"
+                    alt="Generic placeholder image"
+                  />
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
+                    in vulputate at, tempus viverra turpis. Fusce condimentum
+                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+                    in faucibus.
+                  </div>
+                  <div class="row justify-content-end">
+                    <div class="col-4">
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Like
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Comment
+                    </div>
+                  </div>
+                
+                </li>
+                <li class="media my-4">
+                  <img
+                    class="mr-3"
+                    src="maveric-logo-updated.png"
+                    alt="Generic placeholder image"
+                  />
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
+                    in vulputate at, tempus viverra turpis. Fusce condimentum
+                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+                    in faucibus.
+                  </div>
+                  <div class="row justify-content-end">
+                    <div class="col-4">
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Like
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Comment
+                    </div>
+                  </div>
+                </li>
+                <li class="media">
+                  <img
+                    class="mr-3"
+                    src="maveric-logo-updated.png"
+                    alt="Generic placeholder image"
+                  />
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
+                    in vulputate at, tempus viverra turpis. Fusce condimentum
+                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+                    in faucibus.
+                  </div>
+                  <div class="row justify-content-end">
+                    <div class="col-4">
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Like
+                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
+                      Comment
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostPage;
