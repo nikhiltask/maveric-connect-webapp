6a1d154739dfe232a9c2fb985e06138ef4a93bb3
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-25 11:19:37 +0530

    Post Page Structure

--- src/Pages/PostPage/Post.js
+++ src/Pages/PostPage/Post.js
@@ -2,85 +2,90 @@ import React from "react";
 
 const PostPage = () => {
   return (
-    <div class="card">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              <ul class="list-group list-group-flush">
-                <li class="media">
-                  <img
-                    class="mr-3"
-                    src="maveric-logo-updated.png"
-                    alt="Generic placeholder image"
-                  />
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
-                    in vulputate at, tempus viverra turpis. Fusce condimentum
-                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.
-                  </div>
-                  <div class="row justify-content-end">
-                    <div class="col-4">
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Like
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Comment
-                    </div>
-                  </div>
-                
-                </li>
-                <li class="media my-4">
-                  <img
-                    class="mr-3"
-                    src="maveric-logo-updated.png"
-                    alt="Generic placeholder image"
-                  />
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
-                    in vulputate at, tempus viverra turpis. Fusce condimentum
-                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.
-                  </div>
-                  <div class="row justify-content-end">
-                    <div class="col-4">
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Like
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Comment
-                    </div>
-                  </div>
-                </li>
-                <li class="media">
-                  <img
-                    class="mr-3"
-                    src="maveric-logo-updated.png"
-                    alt="Generic placeholder image"
-                  />
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-                    scelerisque ante sollicitudin. Cras purus odio, vestibulum
-                    in vulputate at, tempus viverra turpis. Fusce condimentum
-                    nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.
-                  </div>
-                  <div class="row justify-content-end">
-                    <div class="col-4">
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Like
-                      <img class="mr-1" src="maveric-logo-updated.png" alt="" />
-                      Comment
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img
+            class="mr-3"
+            src="MicrosoftTeams-image (2).png"
+            alt="Generic placeholder image"
+            width="64"
+            height="64"
+          />
+        </div>
+        <div class="col-6">
+          <div class="media-body">
+            <h5 class="mt-0 mb-1">List-based media object</h5>
+            <p>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+              scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+              vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </p>
           </div>
+        </div>
+        <div class="col">
+          <img class="mr-1" ssrc="Like.jpg" width="64" height="64" alt="" />
+          Like
+          <img class="mr-1" src="comment.png" width="64" height="64" alt="" />
+          Comment
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img
+            class="mr-3"
+            src="MicrosoftTeams-image (2).png"
+            alt="Generic placeholder image"
+            width="64"
+            height="64"
+          />
+        </div>
+        <div class="col-6">
+          <div class="media-body">
+            <h5 class="mt-0 mb-1">List-based media object</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+            vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+            vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+        <div class="col">
+          <img class="mr-1" ssrc="Like.jpg" width="64" height="64" alt="" />
+          Like
+          <img class="mr-1" src="comment.png" width="64" height="64" alt="" />
+          Comment
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img
+            class="mr-3"
+            src="MicrosoftTeams-image (2).png"
+            alt="Generic placeholder image"
+            width="64"
+            height="64"
+          />
+        </div>
+        <div class="col-6">
+          <div class="media-body">
+            <h5 class="mt-0 mb-1">List-based media object</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+            vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+            vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+        {/* <div class="col">
+          <img class="mr-1" ssrc="Like.jpg" width="64" height="64" alt="" />
+          Like
+          <img class="mr-1" src="comment.png" width="64" height="64" alt="" />
+          Comment
+        </div> */}
+        <div class="row">
+          <div class="col-8 col-sm-6">Commenter Name</div>
+
+          <div class="col-4 col-sm-6">5 Likes</div>
         </div>
       </div>
     </div>
